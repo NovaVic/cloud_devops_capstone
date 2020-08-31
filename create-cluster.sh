44b7e4b2e632490427ffff6e2ec69b5d838e318a
Suraiya <NovaVic@users.noreply.github.com>
2020-08-30 23:39:47 -0700

    Update create-cluster.sh

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,10 +1,10 @@
 
 # https://docs.aws.amazon.com/eks/latest/userguide/getting-started-eksctl.html
 eksctl create cluster \
---name alphabetsoup \
+--name alphabetsoupv1 \
 --version 1.17 \
 --region us-west-2 \
---nodegroup-name alphabetsoup \
+--nodegroup-name alphabetsoupv1 \
 --node-type t2.micro \
 --nodes 2 \
 --nodes-min 1 \
